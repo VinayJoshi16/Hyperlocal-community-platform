724f9f4b974ed679797e39e5717df3cb4c7b4d67
vinay joshi <vinay.joshi1608@gmail.com>
2026-07-03 09:45:07 +0530

    web setup

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from '../src/redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import App from './App'
+import { store } from './redux/store'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" toastOptions={{ ... }} />
+      </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>
 )
